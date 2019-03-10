22203d353e2657c728357a21c5fa0db8b2b1d5f3
KevNickells <kevin@themindlab.co.uk>
2019-03-10 20:54:49 +0000

    plugins manager

---
+++ .zsh_plugins.sh
@@ -0,0 +1,4 @@
+source /home/kev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/kev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /home/kev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/kev/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
